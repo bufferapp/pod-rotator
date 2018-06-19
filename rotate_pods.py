c06ffac85b7c37e7b03a1d0646115ecfb9231aee
Adnan Issadeen <eddyrox1@hotmail.com>
2018-06-19 21:09:49 +0530

    Changed code to match python conventions

--- rotate_pods.py
+++ rotate_pods.py
@@ -1,15 +1,17 @@
+"""
+Module to route commands when being called.
+
+Is used to call sub modules. This file should only be called
+directly
+"""
 import argparse
-import subprocess
 import rotater
 
 if __name__ == "__main__":
-    parser=argparse.ArgumentParser(description="A Tool to Rotate Pods")
+    parser = argparse.ArgumentParser(description="A Tool to Rotate Pods")
     parser.add_argument("job", choices=['rotate', 'find-secrets', 'mptest'])
     parser.add_argument("remaining", nargs=argparse.REMAINDER)
-    args=parser.parse_args()
-    #print(' '.join(args.remaining))
-    #c = python('rotater.py {}'.format(' '.join(args.remaining)))
-    #print(c)
+    args = parser.parse_args()
     if args.job == "rotate":
         rotater.delete_pods_for_given_deployment(args.remaining)
     elif args.job == "find-secrets":
